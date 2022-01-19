59626bb1a484bb30cfc2dbbe4730c5ec57cad328
Dov Alperin <git@dov.dev>
2022-01-19 10:46:59 -0500

    Make use of Next.js' native code splitting for Katex css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import '@/css/homepage.css'
+import '@/css/admin.css'
+import 'katex/dist/katex.min.css'
 
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
@@ -10,21 +12,24 @@ import siteMetadata from '@/data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
+import { UserProvider } from '@auth0/nextjs-auth0'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </ThemeProvider>
+    <UserProvider>
+      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        {isDevelopment && isSocket && <ClientReload />}
+        <Analytics />
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
+    </UserProvider>
   )
 }
